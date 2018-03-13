26c0a914cdaeadda14a9ca49722b1bf9b7959545
BearCleverProud <xiongconghao@xiongconghaodeMacBook-Pro.local>
2018-03-13 10:14:37 +0800

    Complete three variable gradient descend. Bugs are from the learning rate

--- Chapter2/ThreeVarForLoop.py
+++ Chapter2/ThreeVarForLoop.py
@@ -8,8 +8,7 @@ x2=[]
 x3=[]
 y=[]
 
-with open('/Users/xiongconghao/Downloads/breast-cancer-train.csv', 'r') as f:
-    f.readline()
+with open('breast-cancer-train.csv', 'r') as f:
     line=f.readline()
     while line:
         line=line.strip("\r\n")
@@ -20,39 +19,39 @@ with open('/Users/xiongconghao/Downloads/breast-cancer-train.csv', 'r') as f:
         y.append(int(line[3]))
         line=f.readline()
 
+
 length=len(x1)
 w1=0.0
 w2=0.0
 w3=0.0
 b=0.0
-alpha=0.00001
-for times in range(1,50):
+alpha=0.002978971912446
+for times in range(1,100):
     dw1=0.0
     dw2=0.0
     dw3=0.0
     db=0.0
-    J=0.0
-    for i in range(0,length-1):
+    for i in range(0,length):
         z=w1*x1[i]+w2*x2[i]+w3*x3[i]+b
         a=sigmoid(z)
         dz=a-y[i]
-        J+=-(y[i]*math.log(math.e,a)+(1-y[i])*math.log(math.e,(1-a)))
-        dw1+=x1[i]*dz
-        dw2+=x2[i]*dz
-        dw3+=x3[i]*dz
+        dw1+=dz*x1[i]
+        dw2+=dz*x2[i]
+        dw3+=dz*x3[i]
         db+=dz
-    J/=length
-    dw1/=length
-    dw2/=length
-    dw3/=length
-    db/=length
-    w1-=alpha*dw1
-    w2-=alpha*dw2
-    w3-=alpha*dw3
-    b-=alpha*db
+    dw1=dw1/float(length)
+    dw2=dw2/float(length)
+    dw3=dw3/float(length)
+    db=db/float(length)
+    w1=w1-alpha*dw1
+    w2=w2-alpha*dw2
+    w3=w3-alpha*dw3
+    b=b-alpha*db
 
-while int(input("Do you want to continue test?"))==1:
-    test1=float(input("Input tumor thickness:"))
-    test2=float(input("Input cell size:"))
+resultOffSet=-0.683878244925
+print(w1,w2,w3,b)
+while int(input("Do you want to continue test?:"))==1:
+    test1=float(input("Input first para:"))
+    test2=float(input("Input second para:"))
     test3=float(input("Input final para:"))
-    print(sigmoid(w1*test1+w2*test2+w3*test3+b))
+    print(((sigmoid(w1*test1+w2*test2+w3*test3+b)+resultOffSet)/(1+resultOffSet))**(2))
